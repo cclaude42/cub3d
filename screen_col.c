bfd38efdfa8b495a519c6d89a083389c361c4eb8
YulonSil <richard.alpert.c@gmail.com>
2019-12-14 11:06:00 +0100

    Added pixel finding function for textures (ft_color, commented

--- screen_col.c
+++ screen_col.c
@@ -6,13 +6,36 @@
 /*   By: cclaude <cclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 19:37:22 by cclaude           #+#    #+#             */
-/*   Updated: 2019/12/13 21:52:53 by cclaude          ###   ########.fr       */
+/*   Updated: 2019/12/14 11:05:30 by cclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int		ft_color(t_all *s)
+// unsigned int	ft_color(t_all *s, double i)
+// {
+// 	int	index;
+//
+// 	if (floor(s->hit.y) == s->hit.y)
+// 	{
+// 		index = 64 * floor(64 * i) + 64 * (s->hit.x - floor(s->hit.x));
+// 		if (s->ray.w == 1)
+// 			return (s->tex.s[index]);
+// 		else if (s->ray.w == 0)
+// 			return (s->tex.n[index]);
+// 	}
+// 	else if (floor(s->hit.x) == s->hit.x)
+// 	{
+// 		index = 64 * floor(64 * i) + 64 * (s->hit.y - floor(s->hit.y));
+// 		if (s->ray.v == 1)
+// 			return (s->tex.e[index]);
+// 		else if (s->ray.v == 0)
+// 			return (s->tex.w[index]);
+// 	}
+// 	return (BLACK);
+// }
+
+int				ft_color(t_all *s, double i)
 {
 	if (floor(s->hit.x) == s->hit.x && floor(s->hit.y) == s->hit.y)
 	{
@@ -28,7 +51,7 @@ int		ft_color(t_all *s)
 	return (BLACK);
 }
 
-int		ft_size(t_all *s)
+int				ft_size(t_all *s)
 {
 	double	correc;
 	double	fisheye;
@@ -39,20 +62,20 @@ int		ft_size(t_all *s)
 	return (round(s->win.y / correc));
 }
 
-void	ft_column(t_all *s, int size)
+void			ft_column(t_all *s, int size)
 {
-	int	color;
-	int	start;
-	int	end;
+	unsigned int	color;
+	unsigned int	start;
+	unsigned int	end;
 
 	color = s->tex.c;
 	start = s->win.x * (s->win.y - size) / 2;
 	end = s->win.x * (s->win.y + size) / 2;
 	while (s->ray.i < s->win.x * s->win.y)
 	{
-		if (s->ray.i >= start && s->ray.i <= end)
-			color = ft_color(s);
-		if (s->ray.i = end + 1)
+		if (s->ray.i >= start && s->ray.i < end)
+			color = ft_color(s, (s->ray.i - start) / (s->win.x * size));
+		else if (s->ray.i = end)
 			color = s->tex.f;
 		s->img.adr[s->ray.i] = mlx_get_color_value(s->mlx.ptr, color);
 		s->ray.i += s->win.x;
